c03a846f8d27a4db5f1c435f4908a402ae676392
Samyog Acharya <Samyog.Acharya@gmail.com>
2019-09-19 15:46:08 +0545

    Changing Files names

--- src/main/java/com/example/assignment/repository/PersonDetailRepository.java
+++ src/main/java/com/example/assignment/repository/PersonDetailRepository.java
@@ -9,11 +9,10 @@ import java.util.List;
 @Repository
 public interface PersonDetailRepository extends CrudRepository<PersonDetails, Integer> {
 
-//    @Query("SELECT a.person_detail_id, a.person_general_id,c.email, c.email2, c.first_name, c.last_name ,b.address1, b.address2, b.address3, b.city, b.collateral, b.company_name, b.country_id, b.event_name, b.fax_number, b.job_title, b.note, b.phone_number, b.phone2_number, b.question, b.response, b.scanned_by, b.scanned_date, b.scanned_time, b.state_id, b.status, b.zip FROM person_mapping a, person_details b, person_general_information c WHERE a.person_detail_id=b.person_detail_id and a.person_general_id=c.person_general_id and country_id='United States'")
     @Query("FROM person_details WHERE country_id='United States'")
     List<PersonDetails> findByCountryId();
     List<PersonDetails> findAll();
     List<PersonDetails> findPersonDetailsByStatus(String status);
     List<PersonDetails> findPersonDetailsByScannedBy(String scannedBy);
-
+    
 }
