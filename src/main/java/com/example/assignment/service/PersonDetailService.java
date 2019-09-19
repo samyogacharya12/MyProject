471d95d1ece463cca47128046885859d4fc85c63
Samyog Acharya <Samyog.Acharya@gmail.com>
2019-09-19 15:47:52 +0545

    Changing Files names

--- src/main/java/com/example/assignment/service/PersonDetailService.java
+++ src/main/java/com/example/assignment/service/PersonDetailService.java
@@ -2,11 +2,14 @@ package com.example.assignment.service;
 
 import com.example.assignment.model.PersonDetails;
 
+import java.io.FileNotFoundException;
 import java.util.List;
 
 public interface PersonDetailService {
 
+    void savePersonalDetail(PersonDetails personDetails);
     List<PersonDetails> personGeneralInformationList();
+    void savePersonDetailCSV();
     List<PersonDetails> getListPersonDetails();
     List<PersonDetails> findPersonDetailsByStatus(String status);
     List<PersonDetails> findPersonDetailsByScannedBy(String scannedBy);
