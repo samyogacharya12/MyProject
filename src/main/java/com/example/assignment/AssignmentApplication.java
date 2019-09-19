e7212dd07bc2de5895d501a4bf6e59c1b0df74ca
Samyog Acharya <Samyog.Acharya@gmail.com>
2019-09-19 15:55:51 +0545

    Changing Files names

--- src/main/java/com/example/assignment/AssignmentApplication.java
+++ src/main/java/com/example/assignment/AssignmentApplication.java
@@ -1,13 +1,30 @@
 package com.example.assignment;
 
+import com.example.assignment.service.PersonDetailService;
+import com.example.assignment.service.PersonGeneralService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AssignmentApplication {
+public class AssignmentApplication implements CommandLineRunner {
 
-    public static void main(String[] args) {
+    @Autowired
+    private PersonDetailService personDetailService;
+
+    @Autowired
+    private PersonGeneralService personGeneralService;
+
+    public static void main(String[] args)
+    {
+
         SpringApplication.run(AssignmentApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        personDetailService.savePersonDetailCSV();
+        personGeneralService.savePersonGeneralCSV();;
+    }
 }
